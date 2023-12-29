31a226a671fa1398991980a73a88fea7a0c44e68
Rémy Léone <rleone@scaleway.com>
2023-12-29 15:25:48 +0100

    Update Formula/a/apache-spark.rb
    Co-authored-by: Sean Molenaar <SMillerDev@users.noreply.github.com>

--- Formula/a/apache-spark.rb
+++ Formula/a/apache-spark.rb
@@ -19,7 +19,7 @@ class ApacheSpark < Formula
     sha256 cellar: :any_skip_relocation, all: "86def33303bb1a5c1b9dd388c50889402405166988a9376c94cb547e089c624b"
   end
 
-  depends_on "openjdk@21"
+  depends_on "openjdk"
 
   def install
     # Rename beeline to distinguish it from hive's beeline
